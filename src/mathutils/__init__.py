53de577cad2866e2e89296b9d20580f61b9d74fa
Kayla Marcus <kayla.marc12@gmail.com>
2026-09-24 21:10:55 -0500

    Initial commit

---
+++ src/mathutils/__init__.py
@@ -0,0 +1,5 @@
+"""A tiny math utilities package used for the CI/CD demo."""
+
+from .operations import add, subtract, multiply, divide, is_prime
+
+__all__ = ["add", "subtract", "multiply", "divide", "is_prime"]
