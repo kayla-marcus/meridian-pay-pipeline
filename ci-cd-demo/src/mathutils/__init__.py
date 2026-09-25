bb7110752835346a2ebfb9fc0881cf8c0817027a
Kayla Marcus <kayla.marc12@gmail.com>
2026-09-24 21:27:41 -0500

    Clean up project folder structure

--- ci-cd-demo/src/mathutils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""A tiny math utilities package used for the CI/CD demo."""
-
-from .operations import add, subtract, multiply, divide, is_prime
-
-__all__ = ["add", "subtract", "multiply", "divide", "is_prime"]
